af168bada41d7ad6f05ba6258df8e2076971e417
anlythree <15610462126@163.com>
2023-11-19 21:25:12 +0800

    Initial Commit

---
+++ FoodPicker/FoodPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPickerApp.swift
+//  FoodPicker
+//
+//  Created by anlythree on 2023/11/19.
+//
+
+import SwiftUI
+
+@main
+struct FoodPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
